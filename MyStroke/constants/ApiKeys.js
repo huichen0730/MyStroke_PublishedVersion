092cd9117a10de8ee9f3e062e946b0f285cb33c1
Hui Chen <43622292+huichen0730@users.noreply.github.com>
2019-12-21 12:42:54 +0000

    Update ApiKeys.js

--- MyStroke/constants/ApiKeys.js
+++ MyStroke/constants/ApiKeys.js
@@ -1,11 +1,11 @@
 export default {
     FirebaseConfig:{
-        apiKey: "AIzaSyCezxTzbBWie02Rme15fe3ItOhcIrz_ihY",
-        authDomain: "mystroke.firebaseapp.com",
-        databaseURL: "https://mystroke.firebaseio.com",
+        apiKey: "...",
+        authDomain: "...",
+        databaseURL: "...",
         projectId: "mystroke",
-        storageBucket: "mystroke.appspot.com",
-        messagingSenderId: "1034583424240",
-        appId: "1:1034583424240:web:c8c438f54bffcced"
+        storageBucket: "...",
+        messagingSenderId: "...",
+        appId: "..."
     },
 }
